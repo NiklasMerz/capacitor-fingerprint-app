ac8dde9f8da086c1aeba15bb51d0073d629f77ce
Niklas Merz <niklasmerz@linux.com>
2018-11-13 22:07:04 +0100

    Add fingerprint plugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { FingerprintAIO } from '@ionic-native/fingerprint-aio/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -16,6 +17,7 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    FingerprintAIO,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
+import { FingerprintAIO } from '@ionic-native/fingerprint-aio/ngx';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(public faio: FingerprintAIO) { }
 
+  check() {
+    console.log('check');
+    this.faio.isAvailable().then(result => {
+      console.log(result);
+    }).catch(err => {
+      console.log(err);
+    });
+  }
+
+  show() {
+    console.log('show');
+    this.faio.show({
+      clientId: 'Fingerprint-Demo',
+      clientSecret: 'password', // Only necessary for Android
+      disableBackup: true,  // Only for Android(optional)
+      localizedFallbackTitle: 'Use Pin', // Only for iOS
+      localizedReason: 'Please authenticate' // Only for iOS
+    }).then(result => {
+      console.log(result);
+    }).catch(err => {
+      console.log(err);
+    });
+  }
 }
